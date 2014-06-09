7a47dd10dbfc40ebead5c5f7ced1d4f82f55f388
Ivo Georgiev <ivo@linvo.org>
2014-06-09 18:25:05 +0300

    getFieldIndex: remove stopwords, stemming, metaphone

--- linvodb-fts.js
+++ linvodb-fts.js
@@ -29,7 +29,10 @@ function getDocumentIndex(doc, idxConf)
 	
 };
 
-var tokenizer = new natural.WordTokenizer();
+var tokenizer = new natural.WordTokenizer(),
+	stopwords = _.zipObject(natural.stopwords),
+	stemmer = natural.PorterStemmer.stem,
+	metaphone = natural.Metaphone.process;
 function getFieldIndex(field, fieldConf)
 {
 	/* 
@@ -38,10 +41,11 @@ function getFieldIndex(field, fieldConf)
 	 * 		{ exact: true } disables all those and only indexes exact terms 
 	*/
 	var opts = _.extend({
-		stopwords: true, // false for titles
+		stopwords: true, // false for titles, at least for the exact index
 		stemmer: true,
 		metaphone: true,
-		ngrams: true
+		ngrams: true,
+		boost: 1
 	}, fieldConf || { });
 	
 	var res = { };
@@ -50,13 +54,31 @@ function getFieldIndex(field, fieldConf)
 	 * NOTE: it would be great if we somehow apply this pipeline dynamically
 	 */
 	
-	var tokens = tokenizer.tokenize(field);
-	console.log(tokens);
-	console.log(_.zipObject(natural.stopwords));
-	//.hasOwnProperty()
+	var tokens = tokenizer.tokenize(field), exactTokens;
+	if (opts.title) exactTokens = [].concat(tokens);
+	if (opts.stopwords) tokens = _.filter(tokens, function(t) { return !stopwords.hasOwnProperty(t) });
+	if (!opts.title) exactTokens = [].concat(tokens);
+	
+	if (opts.stemmer) tokens =_.map(tokens, stemmer); // TODO: multi-lingual
+	if (opts.metaphone) tokens = _.map(tokens, function(t) { return metaphone(t) });
+	
+	// idx, idxExact
+	return {
+		idx: getTokensScoring(tokens),
+		//idxBiGram: 
+		//idxTriGram: 
+		idxExact: getTokensScoring(exactTokens),
+		//idxExactBiGram: 
+		//idxExactTriGram:
+	};
 };
 getFieldIndex("polly likes balloons and loves her dog sally's eyes");
 
+function getTokensScoring(tokens, opts)
+{
+	// TODO
+	return _.zipObject(tokens)
+};
 
 function query(indexes, query)
 {
