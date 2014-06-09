e73423a0bd50ad09a269ec21a3f799e8ea6ab9b8
Ivo Georgiev <ivo@linvo.org>
2014-06-09 18:32:14 +0300

    getFieldIndex: n-gram indexes

--- linvodb-fts.js
+++ linvodb-fts.js
@@ -32,7 +32,9 @@ function getDocumentIndex(doc, idxConf)
 var tokenizer = new natural.WordTokenizer(),
 	stopwords = _.zipObject(natural.stopwords),
 	stemmer = natural.PorterStemmer.stem,
-	metaphone = natural.Metaphone.process;
+	metaphone = natural.Metaphone.process,
+	NGrams = natural.NGrams;
+
 function getFieldIndex(field, fieldConf)
 {
 	/* 
@@ -47,9 +49,7 @@ function getFieldIndex(field, fieldConf)
 		ngrams: true,
 		boost: 1
 	}, fieldConf || { });
-	
-	var res = { };
-	
+		
 	/*
 	 * NOTE: it would be great if we somehow apply this pipeline dynamically
 	 */
@@ -62,17 +62,19 @@ function getFieldIndex(field, fieldConf)
 	if (opts.stemmer) tokens =_.map(tokens, stemmer); // TODO: multi-lingual
 	if (opts.metaphone) tokens = _.map(tokens, function(t) { return metaphone(t) });
 	
-	// idx, idxExact
-	return {
-		idx: getTokensScoring(tokens),
-		//idxBiGram: 
-		//idxTriGram: 
-		idxExact: getTokensScoring(exactTokens),
-		//idxExactBiGram: 
-		//idxExactTriGram:
+	var res = {};
+	res.idx = getTokensScoring(tokens);
+	res.idxExact = getTokensScoring(exactTokens);
+	if (opts.ngrams) {
+		var jn = function(t) { return t.join(" ") };
+		res.idxBigram = getTokensScoring(NGrams.bigrams(tokens).map(jn));
+		res.idxTrigram = getTokensScoring(NGrams.trigrams(tokens).map(jn));
+		res.idxExactBigram = getTokensScoring(NGrams.bigrams(exactTokens).map(jn));
+		res.idxExactTrigram = getTokensScoring(NGrams.trigrams(exactTokens).map(jn));
 	};
+	return res;
 };
-getFieldIndex("polly likes balloons and loves her dog sally's eyes");
+console.log(getFieldIndex("polly likes balloons and loves her dog sally's eyes"));
 
 function getTokensScoring(tokens, opts)
 {
