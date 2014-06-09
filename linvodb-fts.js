8b58f80b2079e0bdb3351592e3eec3e8e759cd74
Ivo Georgiev <ivo@linvo.org>
2014-06-09 18:03:19 +0300

    getFieldIndex: basic tokenization

--- linvodb-fts.js
+++ linvodb-fts.js
@@ -29,9 +29,39 @@ function getDocumentIndex(doc, idxConf)
 	
 };
 
+var tokenizer = new natural.WordTokenizer();
 function getFieldIndex(field, fieldConf)
+{
+	/* 
+	 * TODO: shorthands: 
+	 * 		{ title: true } disables stopwords, 
+	 * 		{ exact: true } disables all those and only indexes exact terms 
+	*/
+	var opts = _.extend({
+		stopwords: true, // false for titles
+		stemmer: true,
+		metaphone: true,
+		ngrams: true
+	}, fieldConf || { });
+	
+	var res = { };
+	
+	/*
+	 * NOTE: it would be great if we somehow apply this pipeline dynamically
+	 */
+	
+	var tokens = tokenizer.tokenize(field);
+	console.log(tokens);
+	console.log(_.zipObject(natural.stopwords));
+	//.hasOwnProperty()
+};
+getFieldIndex("polly likes balloons and loves her dog sally's eyes");
+
+
+function query(indexes, query)
 {
 	
 };
+
 
 module.exports = LinvoFTS;
