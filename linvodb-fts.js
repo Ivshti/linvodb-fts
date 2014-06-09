fd01b6545184a07245674c46fbcc3a708ee374c5
Ivo Georgiev <ivo@linvo.org>
2014-06-09 17:46:03 +0300

    Basic initial structure

--- linvodb-fts.js
+++ linvodb-fts.js
@@ -1,1 +1,37 @@
 var natural = require("natural");
+var _ = require("lodash");
+
+/*
+ * TODO: do this in a separate thread?
+ */
+
+function LinvoFTS()
+{
+	var self = this;
+	
+	var indexes = { };
+	
+	/* External interfaces
+	 */
+	self.index = function(doc, idxCfg) {
+		_.merge(indexes, getDocumentIndex(doc, idxCfg));
+	};
+	self.query = function(query, callback) { 
+		
+	};
+	
+	
+	return this;
+};
+
+function getDocumentIndex(doc, idxConf)
+{
+	
+};
+
+function getFieldIndex(field, fieldConf)
+{
+	
+};
+
+module.exports = LinvoFTS;
